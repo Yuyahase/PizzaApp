c50cab9a3913aa9626f982bc8c43ba29724cc84c
haseyuy <haseyuy@gmail.com>
2022-01-31 20:12:13 +0900

    Initial Commit

---
+++ PizzaApp/PizzaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaAppApp.swift
+//  PizzaApp
+//
+//  Created by 長谷侑弥 on 2022/01/31.
+//
+
+import SwiftUI
+
+@main
+struct PizzaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
